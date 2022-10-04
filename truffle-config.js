17dc4058624b321c45b6513440d91223b1133370
John <jastek@SEBEK.localdomain>
2022-10-04 02:24:53 -0700

    truffle-config.js update to Matic RPC URL

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,14 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider") 
+
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+const maticmainnet_rpc_url = 'wss://polygon-mainnet.g.alchemy.com/v2/https://rpc-mainnet.matic.network'
+const maticmumbai_rpc_url = 'https://rpc-mumbai.matic.today'
+
 
 module.exports = {
   networks: {
@@ -9,13 +18,25 @@ module.exports = {
       network_id: "*" // Match any network id
     }
   },
+  maticmainnet: {
+    provider: function() {
+        return new HDWalletProvider(privateKeys, maticmainnet_rpc_url);
+      },
+      network_id: '137',
+  },
+  maticmumbai: {
+    provider: function() {
+        return new HDWalletProvider(privateKeys, maticmumbai_rpc_url);
+      },
+      network_id: '80001',
+  },
 
-  compilers: {
-    solc: {
-      version: '0.8.9',
-      optimizer: {
-        enabled: true,
-        runs: 200
+compilers: {
+solc: {
+version: '0.8.9',
+optimizer: {
+enabled: true,
+runs: 200
       }
     }
   },
